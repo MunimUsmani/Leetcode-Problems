f76478e60d69b046a057dd90fce9ba65461c6d45
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-02-08 14:21:52 +0500

    Create PowerofTwo.java

---
+++ PowerofTwo.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) { // Negative numbers and zero are not power of two
+            return false;
+        }
+        while (n > 1) {
+            if (n % 2 != 0) {
+                return false;
+            }
+            n = n / 2;
+        }
+        return true; // If we reach 1, it's a power of two
+    }
+}
