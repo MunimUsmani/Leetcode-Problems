d2576af99afda27f4f2a213c8b0cb5017bcf7c7d
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-02-10 23:49:18 +0500

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] + nums[j] == target) {
-                    return {i, j}; // Return the indices of the two numbers
+                    return {i, j}; // Return the indices of the two numbers 
                 }
             }
         }
