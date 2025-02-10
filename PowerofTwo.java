dc41ffe7b6c2fc0dfa275761a058f45471347572
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-02-10 23:48:46 +0500

    Leetcode problem no 231

--- PowerofTwo.java
+++ PowerofTwo.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) { // Negative numbers and zero are not power of two
             return false;
-        }
+        } 
         while (n > 1) {
             if (n % 2 != 0) {
                 return false;
