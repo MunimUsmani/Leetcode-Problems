593faad15d91272c187b3c894427f9491c9a4c9e
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-02-10 23:52:07 +0500

    Leetcode problem no 326

--- PowerofThree.java
+++ PowerofThree.java
@@ -5,7 +5,7 @@ class Solution {
         }
         while (n > 1) {
             if (n % 3 != 0) {  
-                return false;
+                return false; 
             }
             n = n / 3;
         }
