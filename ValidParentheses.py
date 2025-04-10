bf518dbc277876c66ef1bd6a35e27139eecf390e
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-04-11 00:27:12 +0500

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,1 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {"(": ")", "{": "}", "[": "]"}
 
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+            elif not stack or lookup[stack.pop()] != parenthese:
+                return False
+
+        return len(stack) == 0
