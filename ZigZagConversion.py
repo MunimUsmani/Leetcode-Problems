6228263dceb392f53d868f4cebdf185fd874dffa
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-04-22 15:10:54 +0500

    Leetcode problem no 6

--- ZigZagConversion.py
+++ ZigZagConversion.py
@@ -1,7 +1,7 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1 or numRows >= len(s):
-            return s
+            return s 
 
         res = ""
         for r in range(numRows):
