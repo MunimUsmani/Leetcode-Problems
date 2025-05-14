8da49779d4df48c190ff800903d6a2407293855b
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-05-14 17:16:55 +0500

    Create AddDigits.py

---
+++ AddDigits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            val = 0
+            while num > 0:
+                val += num % 10
+                num = num // 10
+            num = val
+        return num
