8405d456b996c289c6a45cf4fbabb80bdb9fe353
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-07-18 18:50:53 +0500

    Leetcode problem no 219

---
+++ ContainsDuplicatell.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        nums_map = {}
+
+        for i in range(len(nums)):
+            if nums[i] in nums_map:
+                if abs(i - nums_map[nums[i]]) <= k:
+                    return True
+            nums_map[nums[i]] = i
+
+        return False
