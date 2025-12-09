4da42ce9a61b699239a7ab1e68afdfe736772120
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2025-12-10 00:02:12 +0500

    Leetcode problem no 22
    Implement backtracking algorithm to generate all combinations of well-formed parentheses.

--- GenerateParenthesis.py
+++ GenerateParenthesis.py
@@ -1,1 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(left_count: int, right_count: int, current_string: str) -> None:
 
+            if left_count > n or right_count > n or left_count < right_count:
+                return
+            if left_count == n and right_count == n:
+                result.append(current_string)
+                return
+            backtrack(left_count + 1, right_count, current_string + '(')
+            backtrack(left_count, right_count + 1, current_string + ')')
+      
+        result = []
+        backtrack(0, 0, '')
+      
+        return result
