b3393f48a148b1a5c8f41c4b5bbcfd405aa63882
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2026-02-09 18:21:10 +0500

    Leetcode problem no 7

---
+++ ReverseInteger.js
@@ -0,0 +1,11 @@
+let reverse = function(num) {
+  let reversed = 0;
+
+  while (num > 0) {
+    let digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num = Math.floor(num / 10);
+  }
+
+  return reversed;
+};
